01a1450a091b2db3bdd73c0f8e512a3d92736998
Maneth Pirisyala <mpirisyala2@gmail.com>
2022-03-12 06:49:10 +0530

    first

--- instagramAnalyzer/urls.py
+++ instagramAnalyzer/urls.py
@@ -22,6 +22,7 @@ from users import views as userViews
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("", include('home.urls')),
+    path("projects", include("projects.urls")),
     path('login/', auth_views.LoginView.as_view(template_name="users/login.html"), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name="users/logout.html"), name='logout'),
     path('register/', userViews.register, name='register'),
--- projects/views.py
+++ projects/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from .models import Project
 
-# Create your views here.
+def projects(request):
+    project = request.GET.get("project", "")
+    if project:
+        project = Project.objects.filter(projectId=project).first()
+    else:
+        project = Project.objects.first()
+    return render(
+        request,
+        "projects/projects.html",
+        {
+            "project": project
+        }
+    )
