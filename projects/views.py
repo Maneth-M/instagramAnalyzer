66e52968d67d95124c45950734e317be6cfbb787
Maneth Pirisyala <mpirisyala2@gmail.com>
2022-03-12 10:29:37 +0530

    first

--- projects/views.py
+++ projects/views.py
@@ -1,12 +1,32 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Project
-
+from accounts.models import Account
+from django.contrib import messages
 def projects(request):
     project = request.GET.get("project", "")
+    add = request.GET.get("add", "")
+    remove = request.GET.get("remove", "")
+
     if project:
         project = Project.objects.filter(projectId=project).first()
+        if add:
+            if project.projectAccounts.all().filter(accountId=add).exists():
+                messages.info(request, "Account already added")
+                redirect(f"/projects/?project={project.projectId}")
+            else:
+                project.projectAccounts.add(
+                    Account.objects.filter(accountId=add).first()
+                )
+                project.save()
+                messages.success(request, "Account added")
+                redirect(f"/projects/?project={project.projectId}")
     else:
         project = Project.objects.first()
+    if remove:
+        acc = Account.objects.filter(accountId=remove).first()
+        project.projectAccounts.remove(acc)
+        project.save()
+        messages.success(request, f"{acc.username} Removed")
     return render(
         request,
         "projects/projects.html",
