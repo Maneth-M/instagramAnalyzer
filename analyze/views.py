a86f7ec30cf4c124deee5b87fd08fcd5bff68c70
Maneth Pirisyala <mpirisyala2@gmail.com>
2022-03-14 11:45:27 +0530

    first

--- analyze/views.py
+++ analyze/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
-
+from projects.models import Project
 # Create your views here.
 
 def media (request):
-    return
+    project = request.GET.get('project', "")
+    if project:
+        project = Project.objects.filter(projectId=project).first()
+        accounts = project.projectAccounts.filter(media__datePosted__range=[""])
